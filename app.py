e33e716f41fb31bd7ffcc74e8d40c82d5e759f75
Phoenix-unic <108660054+Phoenix-unic@users.noreply.github.com>
2023-04-13 16:22:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, url_for, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/cover')
+def cover():
+    return render_template('cover.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('sign_up.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
