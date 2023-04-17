98956500c5897b2853c3e96653becebb000aa7cc
Phoenix-unic <108660054+Phoenix-unic@users.noreply.github.com>
2023-04-17 17:59:33 +0300

    day two of doing profile, reworked expandable navbar

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 
 @app.route('/login')
